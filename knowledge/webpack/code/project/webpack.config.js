1d4557b8ae470f97cbd599537a6b553654141d6e
王凌军 <wanglj01@mingyuanyun.com>
2021-02-23 10:55:50 +0800

    webpack：webpack-dev-server开启热更新；1、webpack-cli需求改为3.3.12版本； 2、webpack.config.js需要添加配置项；

--- knowledge/webpack/code/project/webpack.config.js
+++ knowledge/webpack/code/project/webpack.config.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const path = require('path')
+const webpack = require('webpack')
 
 module.exports = {
   // 入口，字符串时为单入口，对象时为多入口
@@ -14,7 +15,8 @@ module.exports = {
     path: path.join(__dirname, 'dist'),
     filename: '[name].js'
   },
-  mode: 'production',
+  // mode: 'production', // 生产
+  mode: 'development', // 开发
   module: {
     rules: [
       {
@@ -47,5 +49,15 @@ module.exports = {
         use: 'file-loader'
       }
     ]
+  },
+  // 引入webpack自带的插件
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  // webpack-dev-server 配置
+  devServer: {
+    contentBase: './dist', // 指定目录
+    hot: true, // 热更新开启
+    host: 'localhost', // 服务器的ip地址
+    port: 25501, // 端口
+    open: true // 自动打开页面
   }
 }
